898ae41408194ec78ee101ea09f329eccb61e7ea
Pepe Suarez <pepesuarez@Pepes-Mac-mini.local>
2021-04-01 17:26:08 -0600

    Added session store model

--- test/models/bank_test.rb
+++ test/models/bank_test.rb
@@ -4,6 +4,7 @@ class BankTest < ActiveSupport::TestCase
   test "It should not create a bank without name" do
     bank = Bank.new
     bank.save
+    puts bank.valid
     assert_not bank.valid?
   end
 
