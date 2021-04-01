65da42be58cb0ed135309a1c7591bedd19428362
Pepe Suarez <pepesuarez@Pepes-Mac-mini.local>
2021-04-01 14:51:02 -0600

    Added initial tests for user creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_042916) do
+ActiveRecord::Schema.define(version: 2021_04_01_203915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_23_042916) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.integer "age"
+    t.integer "points"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  test "It should not create a user without a username and password" do
+    user = User.new password: "Hello"
+    assert_not user.save
+  end
+
+  test "It should create a user correctly" do
+    user = User.new password:"Hello",  username:"John"
+    assert user.save
+  end
 end
